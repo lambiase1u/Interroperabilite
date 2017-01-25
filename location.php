c7878b3c2d5257f81b2278e4ee558b8724213266
lambiase1u <davidlambiase@gmail.com>
2017-01-25 16:47:43 +0100

    add in comments a link for a proxy in location.php add in comments the object we will use to convert xsl in xml.

--- location.php
+++ location.php
@@ -1,5 +1,18 @@
 <?php
 
+// Proxy iut 
+// www-cache:3128
+
+
+/*
+ *  Creation du fichier html a partir d'un XSL
+
+$proc=new XsltProcessor;
+$proc->importStylesheet(DOMDocument::load("test.xsl")); //load script
+echo $proc->transformToXML(DOMDocument::load("test.xml")); //load your file
+
+*/
+
 $data = file_get_contents("http://ip-api.com/xml");
 
 $data_xml = simplexml_load_string($data);
